62411744d4a5b63bc19cd0cb458b037ce3b9c920
Zao_chen <3227442430@qq.com>
2021-11-09 20:22:16 +0800

    Diemessage

---
+++ src/main/java/diemessage/diemessage/commands/maincommands.java
@@ -0,0 +1,62 @@
+package diemessage.diemessage.commands;
+
+import diemessage.diemessage.UpdateChecker;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+
+
+public class maincommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender,Command command, String label,String[] args) {
+        Plugin config = diemessage.diemessage.DieMessage.getProvidingPlugin(diemessage.diemessage.DieMessage.class);
+        File aconfig = new File(diemessage.diemessage.DieMessage.getPlugin(diemessage.diemessage.DieMessage.class).getDataFolder(),"settings.yml");
+        FileConfiguration cconfig = YamlConfiguration.loadConfiguration(aconfig);
+        if (args.length == 0)
+        {
+            sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"/diemessage reload - reload plugin(重载插件)");
+            sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"/diemessage info - look at plugin's info(查看插件配置)");
+        }
+        else
+        {
+            if(args[0].equals("reload")&&sender.hasPermission("diemessage.admin")&&args.length==1)
+            {
+                config.reloadConfig();
+                sender.sendMessage(ChatColor.YELLOW+"[DieMessage] "+ChatColor.GREEN+"Reload succeeded(插件重载成功)");
+
+            }
+            else if(args[0].equals("info"))
+            {
+                if(cconfig.getBoolean("update")) {
+                    new UpdateChecker(diemessage.diemessage.DieMessage.getPlugin(diemessage.diemessage.DieMessage.class), 91658).getVersion(version -> {
+                        if (diemessage.diemessage.DieMessage.getProvidingPlugin(diemessage.diemessage.DieMessage.class).getDescription().getVersion().equalsIgnoreCase(version)) {
+                            sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"The plugin is up to date(插件已是最新版):"+version);
+                        } else {
+                            sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"New version found(发现新版):"+ version+ "\nsuggest to update!https://www.spigotmc.org/resources/diemessage-custom-death-message.91658\n(请前往https://www.mcbbs.net/thread-1194349-1-1.html更新插件");
+                        }
+                    });
+                }
+                if(cconfig.getString("intercept").equals("true")) {
+                    sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"intercept is true(死亡拦截目前开启)");
+                }
+                else sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"intercept is false(死亡拦截目前关闭)");
+                if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null)
+                {
+                    sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"PlaceholderAPI function has been accessed(papi已接入)");
+                }
+                else
+                {
+                    sender.sendMessage(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"PlaceholderAPI cannot be found, PlaceholderAPI feature has been automatically disabled(papi未接入)");
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/diemessage/diemessage/events/Mainevent.java
@@ -0,0 +1,90 @@
+package diemessage.diemessage.events;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+public class Mainevent implements Listener {
+    @EventHandler
+    public void PlayerDeathEvent(PlayerDeathEvent event)
+    {
+        Plugin config = diemessage.diemessage.DieMessage.getProvidingPlugin(diemessage.diemessage.DieMessage.class);
+        File aconfig = new File(diemessage.diemessage.DieMessage.getPlugin(diemessage.diemessage.DieMessage.class).getDataFolder(),"settings.yml");
+        FileConfiguration cconfig = YamlConfiguration.loadConfiguration(aconfig);
+        List<String> list = config.getConfig().getStringList("message");
+        if(Objects.equals(cconfig.getString("intercept"), "true"))
+        {
+            Objects.requireNonNull(event.getEntity().getPlayer()).sendMessage(ChatColor.YELLOW+"+----------+");
+            event.getEntity().getPlayer().sendMessage("Die Message :\n"+event.getDeathMessage());
+            if(event.getEntity().getKiller()!=null) event.getEntity().getPlayer().sendMessage("%killer%: "+event.getEntity().getKiller().getName());
+            event.getEntity().getPlayer().sendMessage("%player%: "+event.getEntity().getPlayer().getName());
+            try
+            {
+                event.getEntity().getPlayer().sendMessage("%arm%"+event.getEntity().getKiller().getEquipment().getItemInMainHand().getItemMeta().getDisplayName() + "");
+            }
+            catch (Exception e)
+            {
+                event.getEntity().getPlayer().sendMessage("%arm%:null");
+
+            }
+            event.getEntity().getPlayer().sendMessage(ChatColor.YELLOW+"+----------+");
+        }
+        int i=0;
+        for(String str : list)
+        {
+            String bi1 = event.getDeathMessage();
+            String bi2 = str.replace("%player%", Objects.requireNonNull(event.getEntity().getPlayer()).getName()+"");
+            if(event.getEntity().getKiller()!=null) {
+                bi2 = bi2.replace("%killer%", event.getEntity().getKiller().getName() + "");
+            }
+            try {
+                bi2 = bi2.replace("%arm%", event.getEntity().getKiller().getEquipment().getItemInMainHand().getItemMeta().getDisplayName() + "");
+            }
+            catch (Exception e)
+            {
+
+            }
+            if (bi1.equals(bi2))
+            {
+                //event.getEntity().getKiller().sendMessage(bi2);
+                String set = list.get(i + 1);
+                if(event.getEntity().getKiller()!=null) set = set.replace("%killer%",event.getEntity().getKiller().getName()+"");
+                try
+                {
+                    set = set.replace("%arm%", event.getEntity().getKiller().getEquipment().getItemInMainHand().getItemMeta().getDisplayName() + "");
+                }
+                catch (Exception e)
+                {
+
+                }
+                if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null)
+                {
+                    Random random_base = new Random();//生成种子
+                    String[] the_split = set.split("\\|");//分割数据
+                    int random_die = random_base.nextInt(the_split.length);//在0-分割数据的数量选取随机数
+                    set=the_split[random_die];//设置随机数输出
+                    event.setDeathMessage(set.replace("%player%", event.getEntity().getPlayer().getName() + ""));
+                    return;
+                }
+                else
+                {
+                    set = PlaceholderAPI.setPlaceholders(event.getEntity().getPlayer(),set);
+                    event.setDeathMessage(set.replace("%player%", event.getEntity().getPlayer().getName() + ""));
+                    return;
+                }
+            }
+            i++;
+        }
+    }
+}
