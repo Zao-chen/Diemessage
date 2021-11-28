59d5321fe2ed2b6a96be4700fd0d2f5307aebf29
Zao_chen <3227442430@qq.com>
2021-11-28 13:06:56 +0800

    更新注解

--- src/main/java/diemessage/diemessage/DieMessage.java
+++ src/main/java/diemessage/diemessage/DieMessage.java
@@ -7,7 +7,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -20,8 +19,18 @@ public final class DieMessage extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         Logger logger = this.getLogger();
+        getServer().getPluginManager().registerEvents(new Mainevent(),this); //加入监听器
+        Objects.requireNonNull(getCommand("diemessage")).setExecutor(new maincommands()); //加入指令
+        int pluginId = 11123; // <-- Replace with the id of your plugin!
+        /*接入state*/
+        Metrics metrics = new Metrics(this, pluginId);
+        /*初始化配置文件*/
+        saveDefaultConfig();
+        /*初始化setting.yml*/
         this.saveResource("settings.yml",false);
-        Plugin config = diemessage.diemessage.DieMessage.getProvidingPlugin(diemessage.diemessage.DieMessage.class);
+        File aconfig = new File(this.getDataFolder(),"settings.yml");
+        FileConfiguration cconfig = YamlConfiguration.loadConfiguration(aconfig);
+        /*检查papi*/
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null)
         {
             logger.info(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"PlaceholderAPI function has been accessed(papi已接入)");
@@ -30,19 +39,14 @@ public final class DieMessage extends JavaPlugin {
         {
             logger.info(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"PlaceholderAPI cannot be found, PlaceholderAPI feature has been automatically disabled(papi未接入)");
         }
-        int pluginId = 11123; // <-- Replace with the id of your plugin!
-        Metrics metrics = new Metrics(this, pluginId);
-        getServer().getPluginManager().registerEvents(new Mainevent(),this);
-        getCommand("diemessage").setExecutor(new maincommands());
-        saveDefaultConfig();
-        File aconfig = new File(this.getDataFolder(),"settings.yml");
-        FileConfiguration cconfig = YamlConfiguration.loadConfiguration(aconfig);
+        /*检查更新*/
         if(cconfig.getBoolean("update")) {
             new UpdateChecker(this, 91658).getVersion(version -> {
-                if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                if (this.getDescription().getVersion().equalsIgnoreCase(version))
+                {
                     logger.info(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"The plugin is up to date(插件已是最新版):"+version);
                 } else {
-                    logger.info(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"New version found(发现新版):"+ version+ "\nsuggest to update!https://www.spigotmc.org/resources/diemessage-custom-death-message.91658\n(请前往https://www.mcbbs.net/thread-1194349-1-1.html更新插件");
+                    logger.info(ChatColor.YELLOW+"[Diemessage] "+ChatColor.GREEN+"New version found(发现新版):"+ version+ "\nYou are still at(你还停留在):"+this.getDescription().getVersion()+"\nsuggest to update!https://www.spigotmc.org/resources/diemessage-custom-death-message.91658\n(请前往https://www.mcbbs.net/thread-1194349-1-1.html更新插件");
                 }
             });
         }
