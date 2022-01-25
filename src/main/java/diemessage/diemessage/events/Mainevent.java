7290cf475cc2b0f0c8afe3baa603809b39322a10
Zao_chen <3227442430@qq.com>
2022-01-25 19:40:18 +0800

    暂时存个档

--- src/main/java/diemessage/diemessage/events/Mainevent.java
+++ src/main/java/diemessage/diemessage/events/Mainevent.java
@@ -32,17 +32,17 @@ public class Mainevent implements Listener {
         {
             Objects.requireNonNull(event.getEntity().getPlayer()).sendMessage(ChatColor.YELLOW+"+----------+");
             event.getEntity().getPlayer().sendMessage("Die Message :\n"+event.getDeathMessage());
-            if(event.getEntity().getKiller()!=null) event.getEntity().getPlayer().sendMessage("%killer%: "+event.getEntity().getKiller().getName());
-            event.getEntity().getPlayer().sendMessage("%player%: "+event.getEntity().getPlayer().getName());
+            if(event.getEntity().getKiller()!=null) event.getEntity().getPlayer().sendMessage("{killer}: "+event.getEntity().getKiller().getName());
+            event.getEntity().getPlayer().sendMessage("{player}: "+event.getEntity().getPlayer().getName());
             try
             {
-                event.getEntity().getPlayer().sendMessage("%arm%: "+ Objects.requireNonNull(Objects.requireNonNull(event.getEntity().getKiller().getEquipment()).getItemInMainHand().getItemMeta()).getDisplayName()
+                event.getEntity().getPlayer().sendMessage("{arm}: "+ Objects.requireNonNull(Objects.requireNonNull(event.getEntity().getKiller().getEquipment()).getItemInMainHand().getItemMeta()).getDisplayName()
                         .replace("§1","").replace("§2","").replace("§3","").replace("§4","").replace("§5","").replace("§6","").replace("§7","").replace("§8","").replace("§9","").replace("§a","").replace("§c","").replace("§d","").replace("§b","").replace("§o","").replace("§l","").replace("§a",""));
                 // 暴力去除彩色属性XD
             }
             catch (Exception e)
             {
-                event.getEntity().getPlayer().sendMessage("%arm%: null");
+                event.getEntity().getPlayer().sendMessage("{arm}: null");
 
             }
             event.getEntity().getPlayer().sendMessage(ChatColor.YELLOW+"+----------+");
@@ -51,12 +51,12 @@ public class Mainevent implements Listener {
         for(String str : list)
         {
             String bi1 = event.getDeathMessage();
-            String bi2 = str.replace("%player%", Objects.requireNonNull(event.getEntity().getPlayer()).getName()+"");
+            String bi2 = str.replace("{player}", Objects.requireNonNull(event.getEntity().getPlayer()).getName()+"");
             if(event.getEntity().getKiller()!=null) {
-                bi2 = bi2.replace("%killer%", event.getEntity().getKiller().getName() + "");
+                bi2 = bi2.replace("{killer}", event.getEntity().getKiller().getName() + "");
             }
             try {
-                bi2 = bi2.replace("%arm%", Objects.requireNonNull(Objects.requireNonNull(event.getEntity().getKiller().getEquipment()).getItemInMainHand().getItemMeta()).getDisplayName()
+                bi2 = bi2.replace("{arm}", Objects.requireNonNull(Objects.requireNonNull(event.getEntity().getKiller().getEquipment()).getItemInMainHand().getItemMeta()).getDisplayName()
                         .replace("§1","").replace("§2","").replace("§3","").replace("§4","").replace("§5","").replace("§6","").replace("§7","").replace("§8","").replace("§9","").replace("§a","").replace("§c","").replace("§d","").replace("§b","").replace("§o","").replace("§l","").replace("§a",""));
                 // 暴力彩色去除
             }
@@ -65,10 +65,10 @@ public class Mainevent implements Listener {
             if (Objects.equals(bi1, bi2))
             {
                 String set = list.get(i + 1);
-                if(event.getEntity().getKiller()!=null) set = set.replace("%killer%",event.getEntity().getKiller().getName()+"");
+                if(event.getEntity().getKiller()!=null) set = set.replace("{killer}",event.getEntity().getKiller().getName()+"");
                 try
                 {
-                    set = set.replace("%arm%", Objects.requireNonNull(Objects.requireNonNull(event.getEntity().getKiller().getEquipment()).getItemInMainHand().getItemMeta()).getDisplayName() + "");
+                    set = set.replace("{arm}", Objects.requireNonNull(Objects.requireNonNull(event.getEntity().getKiller().getEquipment()).getItemInMainHand().getItemMeta()).getDisplayName() + "");
                 }
                 catch (Exception ignored) {}
                 if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null)
@@ -77,7 +77,7 @@ public class Mainevent implements Listener {
                     String[] the_split = set.split("\\|"); //分割数据
                     int random_die = random_base.nextInt(the_split.length); //在0-分割数据的数量选取随机数
                     set=the_split[random_die]; //设置随机数输出
-                    event.setDeathMessage(set.replace("%player%", event.getEntity().getPlayer().getName() + ""));
+                    event.setDeathMessage(set.replace("{player}", event.getEntity().getPlayer().getName() + ""));
                     return;
                 }
                 else //如果没有papi
@@ -87,7 +87,7 @@ public class Mainevent implements Listener {
                     int random_die = random_base.nextInt(the_split.length); //在0-分割数据的数量选取随机数
                     set=the_split[random_die]; //设置随机数输出
                     set = PlaceholderAPI.setPlaceholders(event.getEntity().getPlayer(),set);
-                    event.setDeathMessage(set.replace("%player%", event.getEntity().getPlayer().getName() + ""));
+                    event.setDeathMessage(set.replace("{player}", event.getEntity().getPlayer().getName() + ""));
                     return;
                 }
             }
